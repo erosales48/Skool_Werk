efd14ee884541703df6cb0a7f2a7d570e38c0cf6
erosales48 <157036707+erosales48@users.noreply.github.com>
2024-02-21 22:38:42 -0500

    working initial

---
+++ Lotery Genrator.py
@@ -0,0 +1,51 @@
+# the lottery generator from the textbook
+
+import random
+
+MAX_DIGITS = 5
+START = 1
+END = 70
+MEGA = 25
+
+
+class Color:
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    WHITE = '\033[97m'
+    RESET = '\033[0m'
+
+
+def white_ball():
+    numbers = [0] * MAX_DIGITS
+    for index in range(MAX_DIGITS):
+        numbers[index] = random.randint(START, END)
+    return numbers
+
+
+def megaball():
+    number = random.randint(START, MEGA)
+    return number
+
+
+def main():
+    response = 'y'
+    while response == 'y':
+        numbers = white_ball()
+        mega = megaball()
+        numbers.sort()
+        print('Here are your Lottery numbers:')
+        for index in range(MAX_DIGITS):
+            print(f'{Color.WHITE}{numbers[index]}{Color.RESET}', end=' ')
+
+        print(f'{Color.YELLOW} {mega}{Color.RESET}')
+        response = input('Would you like another? ')
+        response.lower()
+
+
+# standard main call
+if __name__ == "__main__":
+    main()
