09ca4371ac4bf74d861488daa228aa872af173d2
erosales48 <157036707+erosales48@users.noreply.github.com>
2024-02-27 21:36:04 -0500

    Changed name

--- Lotery Generator.py
+++ Lotery Generator.py
@@ -1,12 +1,14 @@
 # the lottery generator from the textbook, improved
 
 import random
+import tkinter as tk
 
 # Constants
 MAX_DIGITS = 5
 START = 1
 END = 70
 MEGA = 25
+
 
 # Color codes in a class for easy use
 class Color:
@@ -23,6 +25,7 @@ class Color:
 # Function to generate 5 distinct numbers within the range given
 def white_ball():
     numbers = [0] * MAX_DIGITS
+    label = ''
     for index in range(MAX_DIGITS):
         accept = False
         while accept is False:
@@ -30,14 +33,22 @@ def white_ball():
             if ball not in numbers:
                 numbers[index] = ball
                 accept = True
-
-    return numbers
+    numbers.sort()
+    for index in range(MAX_DIGITS):
+        label += str(numbers[index]) + ' '
+    return label
 
 
 # the megaball single random nuber is simple
 def megaball():
     number = random.randint(START, MEGA)
     return number
+
+
+def generate_numbers():
+    numbers = white_ball()
+    mega = megaball()
+    result_label.config(text=f"Lottery numbers: {numbers:^16}  Mega Ball: {mega}  ", fg="blue")
 
 
 # Main
@@ -50,7 +61,6 @@ def main():
         numbers = white_ball()
         mega = megaball()
         # Sort the numbers for aesthetics
-        numbers.sort()
         print('Here are your Lottery numbers:')
         # Print the numbers for easy viewing and specify color
         for index in range(MAX_DIGITS):
@@ -62,6 +72,20 @@ def main():
         response.lower()
 
 
+# Main GUI window
+root = tk.Tk()
+root.title("Lottery Number Generator")
+
+# Button to Generate numbers
+generate_button = tk.Button(root, text="Generate Numbers", command=generate_numbers)
+generate_button.pack(pady=10)
+
+# Label to display generated numbers
+result_label = tk.Label(root, text="", fg="green")
+result_label.pack(pady=10)
+
+root.mainloop()
+
 # standard main call
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
